8467cebd1e820a192fe43d9f970719207ec68433
Rome Bell <romebell5@gmail.com>
2021-02-19 14:50:23 -0800

    deploy: add prod config

--- models/index.js
+++ models/index.js
@@ -5,7 +5,18 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+
+let config;
+if (env === 'development') {
+// development
+  config = require(__dirname + '/../config/config.json')[env];
+} else {
+  // prod
+  config = require(__dirname + '/../config/production.json')[env];
+}
+
+
+// production
 const db = {};
 
 let sequelize;
